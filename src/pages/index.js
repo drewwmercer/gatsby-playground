2255a9cbe06e7e167504edfbf72aa18d4f5c20a9
Drew Mercer <japanexpress@gmail.com>
2018-10-18 16:56:47 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Layout from '../components/layout'
 const IndexPage = () => ( <
   Layout >
   <
-  h1 > Hello user < /h1> <
+  h1 > Hello user! < /h1> <
   p > Welcome to the new Gatsby site. < /p> <
   p > Something great is being built. < /p> <
   Link to = "/page-2/" > Go to page 2 < /Link> <
