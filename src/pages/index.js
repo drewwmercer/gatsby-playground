fb7a9801dc13fa92f4c2db6fd2eaa42a7ce8fb8c
Drew Mercer <japanexpress@gmail.com>
2018-10-18 16:40:11 -0400

    Initial setup

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  Link
+} from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => ( <
+  Layout >
+  <
+  h1 > Hello user < /h1> <
+  p > Welcome to a new Gatsby site. < /p> <
+  p > Something great is being built. < /p> <
+  Link to = "/page-2/" > Go to page 2 < /Link> <
+  /Layout>
+)
+
+export default IndexPage
